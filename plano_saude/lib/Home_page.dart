d1f0e7e109377041857f3ab243a1b050f726226a
Cassiolucianoo <cassiolucianodasilva@gmail.com>
2020-09-18 03:16:55 -0300

    Ajustes menu
    Com:
    
    Incluindo no body
    Colunas -  Column
    Linhas - Row
    
    Add de PNG no pubspec
    E icnluido Images no GestureDetector

--- plano_saude/lib/Home_page.dart
+++ plano_saude/lib/Home_page.dart
@@ -5,7 +5,14 @@ class Home_page extends StatefulWidget {
   _Home_pageState createState() => _Home_pageState();
 }
 
+
+
 class _Home_pageState extends State<Home_page> {
+
+  void _abrirHome(){
+
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,6 +22,45 @@ class _Home_pageState extends State<Home_page> {
       ),
       body: Container(
       padding: EdgeInsets.all(16),
+        child:  Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Image.asset("images/Capturar.JPG"),
+            Padding(
+                padding: EdgeInsets.only(top: 30),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: <Widget>[
+                        GestureDetector(
+                           onTap: _abrirHome,
+                          child: Image.asset("images/Menu.png"),
+                        ),
+                        GestureDetector(
+                          onTap: _abrirHome,
+                          child: Image.asset("images/Servicos.png"),
+                        ),
+                      ],
+            ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 32),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  GestureDetector(
+                    onTap: _abrirHome,
+                    child: Image.asset("images/Cliente.png"),
+                  ),
+                  GestureDetector(
+                    onTap: _abrirHome,
+                    child: Image.asset("images/Contato.png"),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
 
     );
